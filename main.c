6c3fc59bb08fa08f5391b1bcc9c1ab1da1095347
Joss Manger <jossferatu@gmail.com>
2020-08-30 21:58:58 -0400

    made more colorful with random seed

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+#define MODULO_MAX 256
 
 int main(int argc, const char **argv)
 {
@@ -16,6 +18,20 @@ int main(int argc, const char **argv)
     black[0] = 0;  /* red */
     black[1] = 0;  /* green */
     black[2] = 0;  /* blue */
+
+    time_t t;
+
+    //seed random
+    srand((unsigned)time(&t));
+
+    //generate random red green and blue sub colors
+    int randomR = rand() % MODULO_MAX;
+    int randomG = rand() % MODULO_MAX;
+    int randomB = rand() % MODULO_MAX;
+
+    //suggested algorithm
+    //"dictionary" - if iteration encountered before, get color
+    // if not, generate new color
 
     FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
     (void) fprintf(fp, "P6\n%d %d\n255\n", width, height);
@@ -35,9 +51,11 @@ int main(int argc, const char **argv)
             if (iteration < max) {
                 int increase = iteration * 100;
                 static unsigned char other[3];
-                other[0] = increase % 256;  /* red */
-                other[1] = 100 % 256;  /* green */
-                other[2] = 200 % 256;  /* blue */
+
+                other[0] = randomR * increase;  /* red */
+                other[1] = randomG * increase;  /* green */
+                other[2] = randomB * increase;  /* blue */
+
                 //printf("iteration: %d other: r:%d, g:%d, b:%d\n",iteration,other[0],other[1],other[2]);
                 (void) fwrite(other, 1, 3, fp);
             } else {
